17898dcc4ad1f68daf5ddeaf52f8e60cc77dbd25
OsamaHu1277 <o.h1277@yahoo.com>
2021-03-03 21:35:24 +0200

    New Look

--- src/compenant/ChatMessages.js
+++ src/compenant/ChatMessages.js
@@ -3,10 +3,11 @@ import './css/ChatMessages.css'
 const ChatMessages = ({ message, auth }) => {
     const { text, uid, photoURL, time } = message
     const MessageStat = uid === auth.currentUser.uid ? 'sent' : 'received';
+    const MessageA = uid === auth.currentUser.uid ? 'sent-A' : 'received-A';
 
     return (
         <div className={`message ${MessageStat}`}>
-            <img className="chat_avatar" src={photoURL} alt=""></img>
+            <img className={`chat_avatar ${MessageA}`} src={photoURL} alt=""></img>
             <p className="text">{text} <br /><span className="date">{time}</span></p>
         </div>
     )
